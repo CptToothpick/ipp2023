15a194c1fbabe85f2999708df0cb2bfbf1f82e72
Martin Laštovica <vonakom@gmail.com>
2023-03-06 00:27:46 +0100

    Basic Class structure

--- parser/parser.php
+++ parser/parser.php
@@ -13,12 +13,76 @@ if($argc > 1){
 
 // Input handling
 
-$input = '';
+$input = array();
 while($line = fgets(STDIN)){
-    $input .= $line;
+    array_push($input,str_replace("\n","",$line));
 }
 
-echo $input[1];
+$code = new Code;
+$code->parseCode($input);
+$code->printCode();
+
+class Instruction{
+    public $opcode;
+    public $args = array();
+
+    public function __construct($opcode)
+    {
+        //TODO: return number of expected args;
+        $this->opcode = $opcode;
+    }
+
+    public function addArg($arg){
+        array_push($this->args,$arg);
+    }
+
+    public function printInstruction(){
+        return "OPCODE:$this->opcode ARGS:" . implode("",$this->args) . "\n";
+    }
+
+}
+
+class Code {
+    public $instructions = array();
+
+    public function printCode(){
+        foreach($this->instructions as $inst){
+            echo $inst->printInstruction();
+        }
+    }
+
+    public function parseCode($input){
+
+        //PARSING LINES INTO INSTRUCTIONS LINE BY LINE
+        foreach($input as $line){
+
+
+            
+            //comment check
+            if($line == "" || $line[0] == "#"){
+                continue;
+            }
+
+            array_push($this->instructions,$this->parseInstruction($line));
+        }
+
+    }
+
+    private function parseInstruction($line){
+
+        //Seperating the line into an array by the " " divider
+        $splicedLine = explode(" ",$line,3);
+
+        //Transforming array into an Argument
+        $instruction = new Instruction($splicedLine[0]);
+        foreach($splicedLine as $arg){
+            if($arg != $instruction->opcode){
+                $instruction->addArg($arg);
+            }
+        }
+        return $instruction;
+    }
+}
 
 
 function printHelp(){
