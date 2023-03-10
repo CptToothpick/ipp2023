3a82d9b7bc2963bc9c90a6e13636744a713c22a9
Martin Laštovica <vonakom@gmail.com>
2023-03-10 11:30:14 +0100

    basic switch

--- parser/parser.php
+++ parser/parser.php
@@ -25,19 +25,74 @@ $code->printCode();
 class Instruction{
     public $opcode;
     public $args = array();
+    private $argsc;
 
     public function __construct($opcode)
     {
         //TODO: return number of expected args;
-        $this->opcode = $opcode;
+        $this->opcode = strtoupper($opcode);
+
+        switch(trim($this->opcode," \n\r\t\v\x00")){
+            case ".IPPCODE23":
+            case "CREATEFRAME":
+            case "PUSHFRAME":
+            case "POPFRAME":
+            case "RETURN":
+            case "BREAK":
+                $argsc = 0;
+                break;
+            case "DEFVAR":
+            case "CALL":
+            case "PUSHS":
+            case "POPS":
+            case "WRITE":
+            case "LABEL":
+            case "JUMP":
+            case "EXIT":
+            case "DPRINT":
+                $argsc = 1;
+                break;
+            case "MOVE":
+            case "INT2CHAR":
+            case "READ":
+            case "TYPE":
+                $argsc = 2;
+                break;
+            case "ADD":
+            case "SUB":
+            case "MUL":
+            case "IDIV":
+            case "LT":
+            case "GT":
+            case "EQ":
+            case "AND":
+            case "OR":
+            case "NOT":
+            case "STRI2INT":
+            case "CONCAT":
+            case "GETCHAR":
+            case "SETCHAR":
+            case "JUMPIFEQ":
+            case "JUMPIFNEQ":
+                $argsc = 3;
+                break;
+            default:
+            error_log("UNKNOWN INSTRUCTION: " . $this->opcode);
+            exit(22);
+        }
     }
 
     public function addArg($arg){
-        array_push($this->args,$arg);
+        if(count($args)<= $argc){
+            array_push($this->args,$arg);
+        }
+        else{
+            exit(23);
+        }
     }
 
     public function printInstruction(){
-        return "OPCODE:$this->opcode ARGS:" . implode("",$this->args) . "\n";
+        return "OPCODE:  $this->opcode ARGS:" . implode(" ",$this->args) . "\n";
     }
 
 }
@@ -56,8 +111,6 @@ class Code {
         //PARSING LINES INTO INSTRUCTIONS LINE BY LINE
         foreach($input as $line){
 
-
-            
             //comment check
             if($line == "" || $line[0] == "#"){
                 continue;
@@ -74,7 +127,9 @@ class Code {
         $splicedLine = explode(" ",$line,3);
 
         //Transforming array into an Argument
+        var_dump($splicedLine);
         $instruction = new Instruction($splicedLine[0]);
+
         foreach($splicedLine as $arg){
             if($arg != $instruction->opcode){
                 $instruction->addArg($arg);
